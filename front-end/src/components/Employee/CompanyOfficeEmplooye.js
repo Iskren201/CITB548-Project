e1b36db1f01410349e47aafd9b0b096d6a474041
Iskren201 <iskren201@gmail.com>
2023-11-29 17:09:14 +0200

    Cart components with 3 card

--- front-end/src/components/Employee/CompanyOfficeEmplooye.js
+++ front-end/src/components/Employee/CompanyOfficeEmplooye.js
@@ -1,8 +1,11 @@
 import React from "react";
+import Cart from "../../Carts/Cart";
 
 const CompanyOfficeEmplooye = () => {
   return (
-    <div className="flex flex-col md:flex-row items-center justify-center mt-4 rounded-lg"></div>
+    <div className="flex flex-col md:flex-row items-center justify-center mt-4 rounded-lg">
+      <Cart />
+    </div>
   );
 };
 
