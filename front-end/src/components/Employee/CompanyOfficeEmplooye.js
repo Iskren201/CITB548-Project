beb501672dbc270252ed697bfeeb1b225f1dc364
Iskren201 <iskren201@gmail.com>
2023-11-29 12:41:27 +0200

    commit

--- front-end/src/components/Employee/CompanyOfficeEmplooye.js
+++ front-end/src/components/Employee/CompanyOfficeEmplooye.js
@@ -1,7 +1,12 @@
 import React from "react";
+import Map from "../Client/googleMap/Map";
 
 const CompanyOfficeEmplooye = () => {
-  return <div>CompanyOfficeEmplooye</div>;
+  return (
+    <div>
+      <Map />
+    </div>
+  );
 };
 
 export default CompanyOfficeEmplooye;
