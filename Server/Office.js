c4fb89c4548ea30ea62e5bc03c6ff91a0d33cdb9
Iskren201 <iskren201@gmail.com>
2024-02-01 22:15:39 +0200

    Update Office.js

--- Server/Office.js
+++ Server/Office.js
@@ -4,6 +4,10 @@ const officeSchema = new mongoose.Schema({
   name: String,
   location: String,
   // Add other fields as needed
+  office: {
+    type: String,
+    required: true,
+  },
 });
 
 const Office = mongoose.model("Office", officeSchema);
